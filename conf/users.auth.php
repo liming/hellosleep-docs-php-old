4af68d63d5b74710edc6548d823a10c2165d713d
Ubuntu <ubuntu@ip-172-31-15-232.ap-northeast-1.compute.internal>
2015-08-03 14:23:47 +0000

    Update 8.2

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -10,3 +10,5 @@
 
 
 match:$1$tT9Wnn3z$XUjhXaJVrz6YgI1qJrmSc0:Ming Li:liming.dl@gmail.com:admin,user
+simple:$1$YNnTeCoX$qWjzyZsHRxXSPcyyjtMCS1:王丹:596039899@qq.com:user
+maggie:$1$BTY9RbI6$Z99.vwnfu/ByuAA/04iY5.:Miao:gmm.dl.cn@gmail.com:user
