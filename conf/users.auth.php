fcfc55c5359a7bd76761c712b0f36b091c0591ea
Ubuntu <ubuntu@ip-172-31-15-232.ap-northeast-1.compute.internal>
2016-02-29 09:15:28 +0000

    Add new articles

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -9,8 +9,8 @@
 # login:passwordhash:Real Name:email:groups,comma,seperated
 
 
-simple:$1$YNnTeCoX$qWjzyZsHRxXSPcyyjtMCS1:王丹:596039899@qq.com:user
 maggie:$1$BTY9RbI6$Z99.vwnfu/ByuAA/04iY5.:Miao:gmm.dl.cn@gmail.com:user
 match:$1$DsRXm8VR$P2Ibriq.NowePH01c.lg4.:Ming Li:liming.dl@gmail.com:admin,user
 xmlrpc:$1$fcaLhtPb$L1bcCWBxzD0/7lJDwnqiq0:XML RPC:xmlrpc@hellosleep.net:user
 anransleep:$1$xvM5TIqz$FOO9cRglc5RgRV.zEUiqa0:安然睡眠:anransleep@163.com:dev
+simple:$1$YNnTeCoX$qWjzyZsHRxXSPcyyjtMCS1:王丹:596039899@qq.com:user,dev
